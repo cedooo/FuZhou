9c3740f6a4f910b388f591b6cb09d2024a2e7b0c
cedoo <cedoo@qq.com>
2015-10-13 15:44:24 +0800

    视图对象

---
+++ src/cn/com/dhcc/fzep/alarm/data/ustc/vo/CurrentAlarm.java
@@ -0,0 +1,66 @@
+package cn.com.dhcc.fzep.alarm.data.ustc.vo;
+
+public class CurrentAlarm {
+
+	private String occurTime;
+	private String alarmContent;
+	private String level;
+	private String equipName;
+	private String currentUser;
+	private String dealTime;
+	private String dealUser;
+	private String note;
+	
+	public String getOccurTime() {
+		return occurTime;
+	}
+	public void setOccurTime(String occurTime) {
+		this.occurTime = occurTime;
+	}
+	public String getAlarmContent() {
+		return alarmContent;
+	}
+	public void setAlarmContent(String alarmContent) {
+		this.alarmContent = alarmContent;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getEquipName() {
+		return equipName;
+	}
+	public void setEquipName(String equipName) {
+		this.equipName = equipName;
+	}
+	public String getCurrentUser() {
+		return currentUser;
+	}
+	public void setCurrentUser(String currentUser) {
+		this.currentUser = currentUser;
+	}
+	public String getDealTime() {
+		return dealTime;
+	}
+	public void setDealTime(String dealTime) {
+		this.dealTime = dealTime;
+	}
+	public String getDealUser() {
+		return dealUser;
+	}
+	public void setDealUser(String dealUser) {
+		this.dealUser = dealUser;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+	
+
+}
---
+++ src/cn/com/dhcc/fzep/alarm/data/ustc/vo/HistoryAlarm.java
@@ -0,0 +1,5 @@
+package cn.com.dhcc.fzep.alarm.data.ustc.vo;
+
+public class HistoryAlarm {
+	
+}
